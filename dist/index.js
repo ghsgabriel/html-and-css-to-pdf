283c0feb39f082c2c8906ed51a3a9e4701ef08f8
Gabriel <ghs.gabe@gmail.com>
2021-11-11 22:25:40 -0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const html_pdf_1 = __importDefault(require("html-pdf"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post('/', (req, res) => {
+    const configs = req.body;
+    const options = { width: configs.width, height: configs.height, border: { top: '0px', bottom: '0px', left: '0px', right: '0px' } };
+    const html = "<html style='margin: 0;padding: 0;border: 0;'>" +
+        "<body style='margin: 0;padding: 0;border: 0;'>" +
+        "<style>@page {margin: 0px;margin-top:0px;sheet-size:708px 1024px;}</style>" +
+        configs.html +
+        "</body></html>";
+    html_pdf_1.default.create(html, options).toBuffer(function (err, buffer) {
+        res.type('application/pdf');
+        res.end(buffer, 'binary');
+    });
+});
+app.listen(3000, () => console.log('On: http://localhost:3000'));
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import pdf, {CreateOptions} from 'html-pdf';
+
+const app = express();
+app.use(express.json());
+
+interface RequestBody {
+    html: string;
+    height: string;
+    width: string;
+}
+
+app.post('/', (req, res) => {
+    const configs = req.body as RequestBody;
+    const options: CreateOptions = {
+        width: configs.width,
+        height: configs.height,
+        border: {top: '0px', bottom: '0px', left: '0px', right: '0px'}
+    };
+    const html = "<html style='margin: 0;padding: 0;border: 0;'>" +
+        "<body style='margin: 0;padding: 0;border: 0;'>" +
+        "<style>@page {margin: 0px;margin-top:0px;}</style>" +
+        configs.html +
+        "</body></html>"
+
+    pdf.create(html, options).toBuffer(function (err, buffer) {
+        res.type('application/pdf');
+        res.end(buffer, 'binary');
+    });
+});
+
+app.listen(3000, () => console.log('On: http://localhost:3000'));
