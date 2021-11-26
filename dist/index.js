b025899af506e5b4732f38b32b03d119ef03c01b
Alper-Testes <74198178+Alper-Testes@users.noreply.github.com>
2021-11-26 11:24:29 -0300

    correção de erro de limite de memoria

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const html_pdf_1 = __importDefault(require("html-pdf"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
+app.use(express_1.default.json({ limit: '50mb' }));
+app.use(express_1.default.urlencoded({ limit: '50mb' }));
 app.post('/', (req, res) => {
     const configs = req.body;
-    const options = { width: configs.width, height: configs.height, border: { top: '0px', bottom: '0px', left: '0px', right: '0px' } };
+    const options = {
+        width: configs.width,
+        height: configs.height,
+        border: { top: '0px', bottom: '0px', left: '0px', right: '0px' }
+    };
     const html = "<html style='margin: 0;padding: 0;border: 0;'>" +
         "<body style='margin: 0;padding: 0;border: 0;'>" +
-        "<style>@page {margin: 0px;margin-top:0px;sheet-size:708px 1024px;}</style>" +
+        "<style>@page {margin: 0px;margin-top:0px;}</style>" +
         configs.html +
         "</body></html>";
     html_pdf_1.default.create(html, options).toBuffer(function (err, buffer) {
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import pdf, {CreateOptions} from 'html-pdf';
 
 const app = express();
-app.use(express.json());
+
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 interface RequestBody {
     html: string;
